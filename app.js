1ba0a7bb29047bbc21fd001c1965e445073461be
MATHEUS LIMA JUNGER <00357580@famacad.com.br>
2026-09-01 21:32:41 -0300

    Adicionando EndPoint /Alunos

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 const express = require('express');
 
+const app = express()
+
+let ALUNOS = [
+    {id: 1, nome: "Adryan", curso : "BD"},
+    {id: 2, nome: "Bruno", curso : "CC"},
+    {id: 3, nome: "Bruna", curso : "ADS"},
+    {id: 4, nome: "Victoria", curso : "SI"},
+];
+
+
+app.get("/", (req, res) => {
+})
+
+
+app.get("/alunos", (req,res) => {
+    res.status(200).json(ALUNOS);
+})
+
+
+const PORTA = 3000
+app.listen(PORTA, ()=>{
+    console.log(`Servidor rodando no http://localhost:${PORTA}`)
+})
+
+// http://localhost:3000/
+// http://localhost:3000/alunos
---
+++ funcao.js
@@ -0,0 +1,17 @@
+function somar(a, b){
+    return a + b;
+}
+
+const somarAnonima = function(a, b){
+    return a + b;
+}
+
+const somaArrowFunction = (a, b) => {
+    return a + b;
+}
+
+
+
+console.log(somar(5,8));
+console.log(somarAnonima(5,8));
+console.log(somaArrowFunction(5,8));
